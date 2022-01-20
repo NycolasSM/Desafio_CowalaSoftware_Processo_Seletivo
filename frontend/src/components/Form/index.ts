73aba20f03f4b100fb4f3f1587182a5a5d5d6723
Nycolas Santana <nycolassantana00@gmail.com>
2022-01-20 17:03:42 -0300

    update, add ip mask

--- frontend/src/components/Form/index.ts
+++ frontend/src/components/Form/index.ts
@@ -1,9 +1,11 @@
 import { Form } from './FormStyle';
 import FormFields from './components/FormFields/FormFields';
+import FormButton from './components/FormButtom/FormButton';
 
 //todo FormFields
 
 export {
   Form as default,
-  FormFields
+  FormFields,
+  FormButton
 }
--- frontend/src/pages/FormUser/FormUser.tsx
+++ frontend/src/pages/FormUser/FormUser.tsx
@@ -1,25 +1,29 @@
 import React, { useState } from 'react';
 import api from '../../services/api';
 
-import Form, { FormFields } from '../../components/Form';
-import { Container } from './FormUserStyle';
+import { convertIPV6ToIPV4 } from './utils/convertIP'
 
-import FormButtom from '../../components/Form/components/FormButtom/FormButton';
+import Form, { FormFields, FormButton } from '../../components/Form';
+import { Container } from './FormUserStyle';
 
 //TODO fazer com que o formulario seja customizavel
 
-//TODO fazer o request da api para o ip
-
 const FormUser: React.FC = () => {
 
-
   const [userIp, setUserIp] = useState("")
+  const getIp = () => {
+    let ip:string
 
-  function getIp():void {
     api.get('/')
     .then(resp => {
-      setUserIp(resp.data)
+      ip = resp.data
     })
+    .then(() =>
+      ip = convertIPV6ToIPV4(ip)
+    )
+    .then(() =>
+      setUserIp(ip)
+    )
   }
 
   return (
@@ -30,10 +34,10 @@ const FormUser: React.FC = () => {
         <FormFields label='Profissão' width={47} name="job" />
         <FormFields label='Celular' width={47} name="phone" />
         <FormFields label='Meu IP' value={userIp} width={63} readonly={true} name="userIP" />
-        <FormButtom onClick={() => getIp()} type='button' label='ENCONTRAR IP' customization={({ backgroundColor: '#00e6de', textColor: 'black', width:'183px' })} />
+        <FormButton onClick={() => getIp()} type='button' label='ENCONTRAR IP' customization={({ backgroundColor: '#00e6de', textColor: 'black', width:'183px' })} />
         <div className='buttomsOptions'>
-          <FormButtom type='submit' label='SALVAR' customization={({ backgroundColor: '#0A1633', textColor: 'white', width:'139px'})}/>
-          <FormButtom type='reset' label='LIMPAR' customization={({ backgroundColor: '#0A1633', textColor: 'white', width:'139px' })}/>
+          <FormButton type='submit' label='SALVAR' customization={({ backgroundColor: '#0A1633', textColor: 'white', width:'139px'})}/>
+          <FormButton type='reset' label='LIMPAR' customization={({ backgroundColor: '#0A1633', textColor: 'white', width:'139px' })}/>
         </div>
       </Container>
     </Form>
